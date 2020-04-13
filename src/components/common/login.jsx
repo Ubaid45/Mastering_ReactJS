0d6ff223866d3452b30f8c2ee85b077f7f6a9324
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-13 18:10:20 +0200

    Validating a form using Joi

--- src/components/common/login.jsx
+++ src/components/common/login.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Joi, { abort } from "joi-browser";
+import Joi from "joi-browser";
 import Input from "./input";
 class Login extends Component {
   state = {
@@ -8,23 +8,21 @@ class Login extends Component {
   };
 
   schema = {
-    username: Joi.string().required(),
-    password: Joi.string().required(),
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
   };
+
   validate = () => {
-    const result = Joi.validate(this.state.account, this.schema, {
+    const options = {
       abortEarly: false,
-    });
-    console.log(result);
+    };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+
+    if (!error) return null;
 
     const errors = {};
-
-    const { account } = this.state;
-    if (account.username.trim() === "")
-      errors.username = "Username is required.";
-    if (account.password.trim() === "")
-      errors.password = "Password is required.";
-    return Object.keys(errors).length === 0 ? null : errors;
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
   };
 
   handleSubmit = (e) => {
