1753fc4cc5588df215ed81d671f3b8507c60d926
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-13 15:35:54 +0200

    control elements by controling them from state and handling multiple inputs

--- src/components/common/login.jsx
+++ src/components/common/login.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 class Login extends Component {
-  userName = React.createRef();
+  state = {
+    account: { username: "", password: "" },
+  };
+
   handleSubmit = (e) => {
     e.preventDefault(); // prevent the default bahviour. Submitting the page to server which cause a full reload
     // Call the server, save the changes ad redirects the user
-    const userName = this.userName.current.value;
-    console.log("username : " + userName);
+    //const userName = this.userName.current.value;
+    console.log("submitted");
   };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -15,16 +26,25 @@ class Login extends Component {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
-              ref={this.userName}
+              value={account.username}
+              onChange={this.handleChange}
               autoFocus
               id="username"
+              name="username"
               type="text"
               className="form-control"
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="password" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              id="password"
+              name="password"
+              type="password"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
