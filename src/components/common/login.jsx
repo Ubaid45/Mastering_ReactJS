b878fdeed21a90620034fc59e7100a74a90fd70c
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-13 23:09:25 +0200

    Extracting a reuseable form

--- src/components/common/login.jsx
+++ src/components/common/login.jsx
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import Joi from "joi-browser";
 import Input from "./input";
-class Login extends Component {
+import Form from "./form";
+class Login extends Form {
   state = {
-    account: { username: "", password: "" },
+    data: { username: "", password: "" },
     errors: {},
   };
 
@@ -12,53 +13,13 @@ class Login extends Component {
     password: Joi.string().required().label("Password"),
   };
 
-  validate = () => {
-    const options = {
-      abortEarly: false,
-    };
-    const { error } = Joi.validate(this.state.account, this.schema, options);
-
-    if (!error) return null;
-
-    const errors = {};
-    for (let item of error.details) errors[item.path[0]] = item.message;
-    return errors;
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault(); // prevent the default bahviour. Submitting the page to server which cause a full reload
-
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
+  doSubmit = () => {
     // Call the server, save the changes and redirects the user
-    //const userName = this.userName.current.value;
     console.log("submitted");
   };
 
-  validateProperty = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema);
-    return error ? error.details[0].message : null;
-  };
-
-  handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    //console.log("error : " + errors[input.name]);
-
-    const errorMessage = this.validateProperty(input);
-    //console.log("error message: " + errorMessage);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-    this.setState({ account, errors });
-  };
-
   render() {
-    const { account, errors } = this.state;
+    const { data, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -66,7 +27,7 @@ class Login extends Component {
           <Input
             name="username"
             label="Username"
-            value={account.username}
+            value={data.username}
             type="text"
             error={errors.username}
             onChange={this.handleChange}
@@ -74,7 +35,7 @@ class Login extends Component {
           <Input
             name="password"
             label="Password"
-            value={account.password}
+            value={data.password}
             type="password"
             error={errors.password}
             onChange={this.handleChange}
