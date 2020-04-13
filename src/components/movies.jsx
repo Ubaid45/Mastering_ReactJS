4034017f0e8332fdccf52736918d01dbe3ac6314
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-14 00:48:36 +0200

    Generic CRUD operations on movies

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,6 +5,7 @@ import ListGroup from "./common/listgroup";
 import Pagination from "./common/pagination";
 import { paginate } from "./utils/paginate";
 import MoviesTable from "./moviesTable";
+import { Link } from "react-router-dom";
 import _ from "lodash";
 
 class Movies extends Component {
@@ -81,6 +82,7 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
+          <Link to="/movies/new" className="btn btn-primary" style={{marginBottom : 20}}>New Movie</Link>
           <p>Showing {totalCount} movies in the database.</p>
           <MoviesTable
             movies={movies}
