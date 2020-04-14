8c36396cfc03022ac6c5e1da1bc94b504b9f001d
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-14 20:50:23 +0200

    Show delete movie column to only admin

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 import Table from "./common/table";
 import Like from "./common/like";
 import { Link } from "react-router-dom";
+import auth from "../services/authService";
 class MoviesTable extends Component {
+  constructor() {
+    super();
+    const user = auth.getCurrentUser();
+    if (user && user.isAdmin) this.columns.push(this.deleteColumn);
+  }
+
   columns = [
     {
       path: "title",
@@ -20,18 +27,20 @@ class MoviesTable extends Component {
         <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
       ),
     },
-    {
-      key: "delete",
-      content: (movie) => (
-        <button
-          onClick={() => this.props.onDelete(movie)}
-          className="btn btn-danger btn-sm"
-        >
-          Delete
-        </button>
-      ),
-    },
   ];
+
+  deleteColumn = {
+    key: "delete",
+    content: (movie) => (
+      <button
+        onClick={() => this.props.onDelete(movie)}
+        className="btn btn-danger btn-sm"
+      >
+        Delete
+      </button>
+    ),
+  };
+
   render() {
     const { movies, onSort, sortColumn } = this.props;
     return (
