5e2f967516728de5980fed7209303079c4c73c1f
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-14 14:33:21 +0200

    Replaced fakeMovieService with node backend service

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import { getMovies } from "../services/fakeMovieService";
+import { getMovies, deleteMovie } from "../services/movieService";
 import { getGenres } from "../services/genreService";
 import ListGroup from "./common/listgroup";
 import Pagination from "./common/pagination";
 import { paginate } from "./utils/paginate";
 import MoviesTable from "./moviesTable";
+import { toast } from "react-toastify";
 import { Link } from "react-router-dom";
 import _ from "lodash";
 import SearchBox from "./searchBox";
@@ -20,9 +21,11 @@ class Movies extends Component {
     sortColumn: { path: "title", order: "asc" },
   };
   async componentDidMount() {
-    const {data} = await getGenres(); 
+    const { data } = await getGenres();
     const genres = [{ _id: "", name: "All Genres" }, ...data];
-    this.setState({ movies: getMovies(), genres });
+
+    const { data: movies } = await getMovies();
+    this.setState({ movies, genres });
   }
 
   handleLike = (movie) => {
@@ -42,17 +45,28 @@ class Movies extends Component {
     this.setState({ currentPage: page });
   };
 
-  handleDelete = (movie) => {
+  handleDelete = async (movie) => {
     //console.log(movie);
+    const originalMovies = this.state.movies;
+
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
     this.setState({ movies });
+
+    try {
+      await deleteMovie(movie._id);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        toast.error("This movie has already been deleted.");
+
+      this.setState({ movies: originalMovies });
+    }
   };
 
   handleSort = (sortColumn) => {
     this.setState({ sortColumn });
   };
 
-  handleSearch = query => {
+  handleSearch = (query) => {
     this.setState({ searchQuery: query, selectedGenre: null, currentPage: 1 });
   };
 
@@ -63,16 +77,16 @@ class Movies extends Component {
       sortColumn,
       selectedGenre,
       searchQuery,
-      movies: allMovies
+      movies: allMovies,
     } = this.state;
 
     let filtered = allMovies;
     if (searchQuery)
-      filtered = allMovies.filter(m =>
+      filtered = allMovies.filter((m) =>
         m.title.toLowerCase().startsWith(searchQuery.toLowerCase())
       );
     else if (selectedGenre && selectedGenre._id)
-      filtered = allMovies.filter(m => m.genre._id === selectedGenre._id);
+      filtered = allMovies.filter((m) => m.genre._id === selectedGenre._id);
 
     const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
 
@@ -98,7 +112,13 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <Link to="/movies/new" className="btn btn-primary" style={{marginBottom : 20}}>New Movie</Link>
+          <Link
+            to="/movies/new"
+            className="btn btn-primary"
+            style={{ marginBottom: 20 }}
+          >
+            New Movie
+          </Link>
           <p>Showing {totalCount} movies in the database.</p>
           <SearchBox value={searchQuery} onChange={this.handleSearch} />
           <MoviesTable
